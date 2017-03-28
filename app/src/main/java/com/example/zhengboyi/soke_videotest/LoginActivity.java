a5f0257ee459a28ed39a95973f4fda06041bba91
lgezzby <634065623@qq.com>
2017-03-28 09:41:27 +0800

    配置WebServiceGet.java(HttpURLConnection实现，还未实现接口)

--- app/src/main/java/com/example/zhengboyi/soke_videotest/LoginActivity.java
+++ app/src/main/java/com/example/zhengboyi/soke_videotest/LoginActivity.java
@@ -1,19 +1,14 @@
 package com.example.zhengboyi.soke_videotest;
 
-import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import org.json.JSONObject;
-
-import java.net.HttpURLConnection;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Login 登录界面
@@ -22,8 +17,15 @@ import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     //定义文本框、按钮、复选框、超链接
-    EditText et_username,et_password;
+    EditText et_username, et_password;
     Button btn_login;
+    ProgressDialog progressDialog;
+
+    // 返回的数据
+    private String info;
+    // 返回主线程更新数据
+    private static Handler handler = new Handler();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,47 +43,52 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_login:
-                if (validate()){
-                    if (loginPro()){
-
-                    }
-                    else{
-                        
-                    }
+                if (validate()) {
+                    progressDialog = new ProgressDialog(this);
+                    progressDialog.setTitle("提示");
+                    progressDialog.setMessage("正在登陆，请稍后...");
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+                    // 创建子线程，分别进行Get和Post传输
+                    new Thread(new MyThread()).start();
                 }
                 break;
         }
     }
 
+    /*
+    * 检验用户名和密码是否符合要求
+    * 1、用户名,密码不能为空
+    * */
     private boolean validate() {
         String username = et_username.getText().toString().trim();
-        if (username.equals("")){
-            Toast.makeText(this,"用户账户是必填项!",Toast.LENGTH_SHORT).show();
+        if (username.equals("")) {
+            Toast.makeText(this, "用户账户是必填项!", Toast.LENGTH_SHORT).show();
             return false;
         }
         String password = et_password.getText().toString().trim();
-        if (password.equals("")){
-            Toast.makeText(this,"用户口令是必填项!",Toast.LENGTH_SHORT).show();
+        if (password.equals("")) {
+            Toast.makeText(this, "用户口令是必填项!", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
     }
 
-    private boolean loginPro() {
-        String username = et_username.getText().toString();
-        String password = et_password.getText().toString();
-        return false;
-    }
+    // 子线程接收数据，主线程修改数据
+    public class MyThread implements Runnable {
 
-    private JSONObject query(String username, String password){
-        //使用Map封装请求参数
-        Map<String,String> map = new HashMap<String, String>();
-        map.put("username",username);
-        map.put("password",password);
-        //定义发送请求的URL
-        String url = "";
-        return new JSONObject();
+        @Override
+        public void run() {
+            info = WebServiceGet.executeHttpGet(et_username.getText().toString(), et_password.getText().toString());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(LoginActivity.this,info,Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            });
+        }
     }
 }
